456ab452f59e910f4ad335b12695dde0c5f4e0d2
Juan Camilo <jccifuentes21@gmail.com>
2022-09-28 11:48:59 -0700

    fixed leaked mongo password

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -2,13 +2,15 @@
 
 import { MongoClient } from "mongodb";
 
+const mongoPw= process.env.REACT_APP_MongoDB_PW
+
 //This code only runs in the server side!!! It is safe to store credentials
 const handler = async (req, res) => {
   if (req.method === "POST") {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      "mongodb+srv://jccifuentes21:c7qazqiFLWBdTg8X@cluster0.qxm2z48.mongodb.net/meetups?retryWrites=true&w=majority"
+      `mongodb+srv://jccifuentes21:${mongoPw}@cluster0.qxm2z48.mongodb.net/meetups?retryWrites=true&w=majority`
     );
     const db = client.db();
 
